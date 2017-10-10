3ad4024c8131bf93d511308f4cbde3093fa7be97
Kevin <kevin.gallagher23@gmail.com>
2017-10-10 15:27:52 -0400

    app changes

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require 'sinatra'
 require 'sendgrid-ruby'
+<<<<<<< HEAD
 require 'dotenv/load'
+=======
+
+before do
+	@class =""
+end
+>>>>>>> 091f33dc375ed82705b4fb4353b351d5936e941f
 
 get '/' do
+	@class = "home"
 	erb :home
 
 end
